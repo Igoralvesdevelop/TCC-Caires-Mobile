1e21b42790ce6ebf84b5c0003f6ea8b72be6c1f8
Igor Alves de Santana <167438916+Igoralvesdevelop@users.noreply.github.com>
2025-04-09 20:40:24 -0300

    perfil2

---
+++ Caires-Mobile/App.tsx
@@ -0,0 +1,26 @@
+import './gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/Login';
+import Home from './src/pages/home';
+import BottomRoutes from './src/routes/botton.routes';
+import { NavigationContainer } from '@react-navigation/native';
+export default function App() {
+  return (
+     <NavigationContainer>
+       <BottomRoutes />
+     </NavigationContainer>
+   // <Login/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height:'100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
